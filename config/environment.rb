dc871004c17b60c67c5f6c385f1a8f66fb049978
chintas1 <chintas1@tcnj.edu>
2016-02-22 07:57:09 -0500

    working console.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,12 @@
-require_all 'app'
-require 'sqlite3'
+require 'bundler/setup'
+Bundler.require
 
-DB = {:conn => SQLite3::Database.new("db/movieapp.db")}
+DB = {:conn => SQLite3::Database.new("movieapp.db")}
 
 DB[:conn].results_as_hash = true
+
+def exec(query)
+  DB[:conn].execute(query)
+end
+
+require_all 'app'
